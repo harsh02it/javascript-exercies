ff5c3b97696dbef30baa730fc1bff9486b7db050
harsh02it <harshitsavani@gmail.com>
2025-02-17 19:50:38 -0500

    added an HTML page to check the outputs of each exercise

---
+++ exercise-1.js
@@ -0,0 +1,13 @@
+//Declare variables using var, let, const and log them to the console.
+
+let x1 = 6;
+let x2 = "Hello World";
+let x3 = true;
+let x4 = null;
+let x5 = undefined;
+
+console.log("x1:", x1);
+console.log("x2:", x2);
+console.log("x3:", x3);
+console.log("x4:", x4);
+console.log("x5:", x5);
